69442bb8327e9134883a9f4b7fe814e83ed71535
= <andyserrano_d@outlook.com>
2023-06-12 21:59:36 -0600

    amplify + twilio

---
+++ amplify/backend/function/movieapp56fba7f8/src/index.js
@@ -0,0 +1,19 @@
+
+
+/**
+ * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
+ */
+const accountSid = 'ACd0cd5cf0c35391b4f2c68358145b01cf';
+const authToken = '[AuthToken]';
+const client = require('twilio')(accountSid, authToken);
+exports.handler = async (event) => {
+    console.log(`EVENT: ${JSON.stringify(event)}`);
+    client.messages
+    .create({
+        body: 'Tu orden ha sido recibida. Esperemos que lo disfrutes Andy. Detalles: http://www.yummycupcakes.com/',
+        from: 'whatsapp:+14155238886',
+        to: 'whatsapp:+5215551670769'
+    })
+    .then(message => console.log(message.sid))
+    .done();
+};
---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,18 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "api375b2f21": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
+  "function": {
+    "movieapp56fba7f8": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  }
+}
